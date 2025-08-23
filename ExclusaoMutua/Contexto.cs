37101d8d99db751ca432dce91603c661c108d097
Vinícius Souza <vinigsouza.bnu@gmail.com>
2025-08-22 23:13:25 -0300

    work in progress

---
+++ ExclusaoMutua/Contexto.cs
@@ -0,0 +1,70 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace ExclusaoMutua
+{
+    public class Contexto
+    {
+        public IPAddress IpAddress { get; set; }
+        //public List<Processo> Processos { get; set; } = new();
+
+        public Dictionary<int, Thread> Processos { get; set; }
+        public int CoordenadorId { get; set; }
+        public Recurso Recurso { get; set; }
+
+        public Contexto()
+        {
+            IpAddress = IPAddress.Parse("127.0.0.1");
+            Recurso recurso = new Recurso
+            {
+                Id = 1,
+                Nome = "Recurso 1"
+            };
+            // cria uma thread
+            CoordenadorId = 1;
+            Thread criadorProcessos = new Thread(CriaProcessos);
+        }
+
+        public void CriaProcessos()
+        {
+            var contador = 1;
+
+            while (true)
+            {
+                var processo = new Processo(contador, CoordenadorId, Recurso);
+                Thread thread = new Thread(() => processo.IniciaServidor(IpAddress));
+                Processos.Add(contador, thread);
+                contador++;
+                Thread.Sleep(40000);
+            }
+        }
+
+        public void DefineCoordenador()
+        {
+            var coordenadorId = Processos
+                .OrderBy(x => Guid.NewGuid())
+                .FirstOrDefault();
+
+            
+            foreach (var processo in Processos)
+            {
+               // chamar processos passando coordenadorId
+            }
+        }
+
+        public void MataCoordenador()
+        {
+            Thread.Sleep(60000);
+            if (Processos.ContainsKey(CoordenadorId))
+            {
+                var coordenadorThread = Processos[CoordenadorId];
+                if (coordenadorThread.IsAlive)
+                {
+                    coordenadorThread.Abort(); // Aborta a thread do coordenador
+                }
+                Processos.Remove(CoordenadorId);
+            }
+            DefineCoordenador();
+        }
+    }
+}
---
+++ ExclusaoMutua/Program.cs
@@ -0,0 +1,166 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net;
+using System.Net.Sockets;
+
+Console.WriteLine("Hello, World!");
+
+
+// processo
+
+// recurso
+
+// coordenador
+
+// requisicao
+// contem processo e recurso
+
+public class Processo
+{
+    public int Id { get; set; }
+    public bool SouCoordenador { get; set; }
+    public int Coordenador { get; set; }
+    public bool AguardandoRecurso { get; set; }
+    public Queue<Mensagem> FilaRequisicoes { get; set; } = new();
+    // seria uma lista de recursos, mas para simplificar
+    public Recurso Recurso { get; set; }
+    public Thread ThreadRecurso { get; set; }
+
+    public Processo(int id, int coordenadorId, Recurso recurso)
+    {
+        Id = id;
+        Coordenador = coordenadorId;
+        Recurso = recurso;
+    }
+
+    public void IniciaServidor(IPAddress ipAddress)
+    {
+        TcpListener listener = new TcpListener(ipAddress, 5000 + Id);
+        listener.Start();
+        while (true)
+        {
+            TcpClient client = listener.AcceptTcpClient();
+        }
+    }
+
+    public void RecebeCoordenador(int coordenadorId)
+    {
+        Coordenador = coordenadorId;
+        SouCoordenador = Id == coordenadorId;
+    }
+
+    public void IniciaRequisitador()
+    {
+        var random = new Random();
+        while (true)
+        {
+            if (!AguardandoRecurso)
+            {
+                SolicitaRecurso(Recurso);
+            }
+            Thread.Sleep(random.Next(10000, 25000));
+        }
+    }
+
+    public void SolicitaRecurso(Recurso recurso)
+    {
+        if (AguardandoRecurso)
+            return;
+        var requisicao = new Mensagem
+        {
+            Tipo = ETipoMensagem.Requisicao,
+            RecursoId = recurso.Id,
+            ProcessoId = Id
+        };
+        // envia requisicao para o coordenador
+        AguardandoRecurso = true;
+    }
+
+    public void RecebeMensagem(Mensagem mensagem)
+    {
+        switch (mensagem.Tipo)
+        {
+            case ETipoMensagem.Requisicao:
+                if (SouCoordenador)
+                {
+                    if (!Recurso.EmUso)
+                    {
+                        var concessao = new Mensagem
+                        {
+                            Tipo = ETipoMensagem.Concessao,
+                            RecursoId = mensagem.RecursoId,
+                            ProcessoId = mensagem.ProcessoId
+                        };
+                        // envia concessao
+                        Recurso.EmUso = true;
+                    }
+                    else
+                    {
+                        FilaRequisicoes.Enqueue(mensagem);
+                    }
+                }
+                else
+                {
+                    // perfume - responde que não é
+                }
+                    break;
+            case ETipoMensagem.Concessao:
+                if (AguardandoRecurso && mensagem.RecursoId == 1) // exemplo
+                {
+                    AguardandoRecurso = false;
+                    var random = new Random(); // melhorar
+                    var tempoUso = random.Next(5000, 15000);
+                    Thread.Sleep(tempoUso);
+                    var liberacao = new Mensagem
+                    {
+                        Tipo = ETipoMensagem.Liberacao,
+                        RecursoId = mensagem.RecursoId
+                    };
+                    // envio pro coordenador
+                }
+                break;
+            case ETipoMensagem.Liberacao:
+                if (SouCoordenador)
+                {
+                    Recurso.EmUso = false;
+                    // libera o recurso
+
+                    // se tiver alguem na fila, concede
+                    if (FilaRequisicoes.Count > 0)
+                    {
+                        var prox = FilaRequisicoes.Dequeue();
+                        var concessao = new Mensagem
+                        {
+                            Tipo = ETipoMensagem.Concessao,
+                            RecursoId = prox.RecursoId,
+                            ProcessoId = prox.ProcessoId
+                        };
+                        // envia concessao
+                        Recurso.EmUso = true;
+                    }
+                }
+                break;
+        }
+    }
+}
+
+public class Recurso
+{
+    public long Id { get; set; }
+    public string Nome { get; set; }
+    public bool EmUso { get; set; }
+    // qualquer outro atributo que seja necessário
+}
+
+public class Mensagem
+{
+    public ETipoMensagem Tipo { get; set; }
+    public long RecursoId { get; set; }
+    public int ProcessoId { get; set; }
+}
+
+public enum ETipoMensagem
+{
+    Requisicao,
+    Concessao,
+    Liberacao,
+}
